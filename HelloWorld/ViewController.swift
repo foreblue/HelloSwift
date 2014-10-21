600b09086d37b3470001d8814a8edef8597082a5
dy.sim <dysim@dysim-ui-Mac.local>
2014-10-21 16:44:41 +0900

    init

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    var audioPlayer: AVAudioPlayer?
+    var progressTimer: NSTimer?
+    
+    @IBOutlet var progressSlider: UISlider!
+    @IBOutlet var volumeStepper: UIStepper!
+    @IBOutlet var volumeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
+        var bundleURL: NSURL = NSBundle.mainBundle().URLForResource("demo", withExtension: "mp3")!
+        self.audioPlayer = AVAudioPlayer(contentsOfURL: bundleURL, error: nil)
+        self.audioPlayer!.delegate = self
+        self.progressSlider!.value = 0
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,7 +32,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func clickPlayButton(button: UIButton) {
+        self.audioPlayer!.play()
+        makeProgressTimer()
+    }
 
+    @IBAction func clickStopButton(button: UIButton) {
+        self.audioPlayer!.stop()
+        invalidateProgressTimer()
+    }
 
+    @IBAction func clickVolumeStepper(stepper: UIStepper) {
+        self.audioPlayer!.volume = CFloat(stepper.value)
+        self.volumeLabel!.text = NSString(format: "%.1f", stepper.value)
+    }
+    
+    func makeProgressTimer() {
+        self.progressTimer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("checkCurrentTime"), userInfo: nil, repeats: true)
+    }
+    
+    func invalidateProgressTimer() {
+        self.progressTimer!.invalidate()
+    }
+    
+    func checkCurrentTime() {
+        self.progressSlider.value = CFloat(self.audioPlayer!.currentTime / self.audioPlayer!.duration)
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+        invalidateProgressTimer()
+    }
 }
-
